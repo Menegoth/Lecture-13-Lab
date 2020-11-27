46954b15ebe8a2ddf76bdc79a15cc30efdef58a4
Antonio <antonioareas@me.com>
2020-11-27 12:27:41 -0800

    Add project files.

---
+++ Lecture 13 Lab/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lecture_13_Lab {
+    class Rectangle {
+        static void Main(string[] args) {
+
+            Rectangle rect = new Rectangle();   
+            MyDelegate rectDele = new MyDelegate(rect.Area);
+            rectDele += new MyDelegate(rect.Perimeter);
+            rectDele.Invoke(6.0, 4.5);
+            rectDele.Invoke(12.0, 6.4);
+
+        }
+
+        public delegate void MyDelegate(double height, double width);
+
+        public void Area (double height, double width) {
+            Console.WriteLine("Area is: " + height * width);
+        }
+
+        public void Perimeter (double height, double width) {
+            Console.WriteLine("Perimeter is: " + (height * 2) + (width * 2));
+        }
+
+    }
+}
